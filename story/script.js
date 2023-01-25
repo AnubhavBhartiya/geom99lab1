26db6bd431cb3573c48508948754bc592fdbddf2
AnubhavBhartiya <114168671+AnubhavBhartiya@users.noreply.github.com>
2023-01-25 14:09:05 -0500

    Update script.js

--- story/script.js
+++ story/script.js
@@ -41,35 +41,35 @@ function initMap() {
       var StopMarkers = new google.maps.Marker({
       position: tourStops[0],
       map: map,
-      icon: "https://i.ibb.co/WxQFxD6/pin-house-logo-icon-design-vector-22734497-removebg-preview.png"
+      // icon: "https://i.ibb.co/WxQFxD6/pin-house-logo-icon-design-vector-22734497-removebg-preview.png"
       });
             // Create the markers.
             var StopMarkers
  = new google.maps.Marker({
                   position: {lat: 22.920, lng: 75.854, name: 'Indore'},
                   map: map,
-                  icon: "https://i.ibb.co/WxQFxD6/pin-house-logo-icon-design-vector-22734497-removebg-preview.png"
+                  // icon: "https://i.ibb.co/WxQFxD6/pin-house-logo-icon-design-vector-22734497-removebg-preview.png"
                   });
             // Create the markers.
             var StopMarkers
  = new google.maps.Marker({
                   position: {lat: 28.551, lng: 77.390, name: 'Noida'},
                   map: map,
-                  icon: "https://i.ibb.co/WxQFxD6/pin-house-logo-icon-design-vector-22734497-removebg-preview.png"
+                  // icon: "https://i.ibb.co/WxQFxD6/pin-house-logo-icon-design-vector-22734497-removebg-preview.png"
                   });
             // Create the markers.
             var StopMarkers
 = new google.maps.Marker({
                   position: {lat: 28.407, lng: 77.325, name: 'Faridabad'},
                   map: map,
-                  icon: "https://i.ibb.co/WxQFxD6/pin-house-logo-icon-design-vector-22734497-removebg-preview.png"
+                  // icon: "https://i.ibb.co/WxQFxD6/pin-house-logo-icon-design-vector-22734497-removebg-preview.png"
                   });
             // Create the markers.
             var StopMarkers
 = new google.maps.Marker({
                   position: {lat: 44.356, lng: -78.736, name: 'Lindsay'},
                   map: map,
-                  icon: "https://i.ibb.co/WxQFxD6/pin-house-logo-icon-design-vector-22734497-removebg-preview.png"
+                  // icon: "https://i.ibb.co/WxQFxD6/pin-house-logo-icon-design-vector-22734497-removebg-preview.png"
                   });
   
         // Add a click listener for each marker, and set up the info window.
